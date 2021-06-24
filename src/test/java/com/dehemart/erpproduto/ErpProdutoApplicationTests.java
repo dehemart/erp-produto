7b9d21d5d231ea2c433f31e90521934957ae3ee8
dehemart <martins.denis@gmail.com>
2021-06-24 18:06:23 -0300

    erp-produto - initial

---
+++ src/test/java/com/dehemart/erpproduto/ErpProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dehemart.erpproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpProdutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
